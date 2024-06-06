b883b1e10a48afad7a905c055d318cc03087bde6
Yash <yashsavaliya1103@gmail.com>
2024-06-06 16:40:28 +0530

    show photos

--- init/data.js
+++ init/data.js
@@ -29,7 +29,7 @@ const sampleListings = [
         "Unplug and unwind in this peaceful mountain cabin. Surrounded by nature, it's a perfect place to recharge.",
       image: {
         filename: "listingimage",
-        url: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+        url: "https://www.freepik.com/free-photo/movie-night-by-pool-whole-family_136729237.htm#query=villa&position=8&from_view=keyword&track=sph&uuid=8f53b1b0-f164-4733-a27c-634f17f37449",
       },
       price: 1000,
       location: "Aspen",
